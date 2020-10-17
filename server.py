b6f38590e05442fc02e51a1ffc6cabd521090288
Karel Smutny <karel.smutny@outlook.com>
2020-10-17 15:09:58 +0200

    Implemented servo control

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
+from PCA9685 import PCA9685
+
+
+pwm = PCA9685(0x40)
+pwm.setPWMFreq(50)
+
+POS_INIT = 1500
+POS_ON = POS_INIT
+POS_OFF = 2200
+
+
+def initialize_servos():
+    global pwm
+    for id in range(0, 16):
+        pwm.setServoPulse(id, POS_INIT)
+
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        global pwm
+
         content_type = self.headers['Content-Type']
         content_length = int(self.headers['Content-Length'])
 
@@ -16,6 +34,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         on = content['on']
 
         print('Switching switch', id, 'on' if on else 'off')
+        pwm.setServoPulse(id, POS_ON if on else POS_OFF)
 
         self.send_response(201)
         self.end_headers()
@@ -26,7 +45,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 
 
-def run():
+def start_server():
     port = 8000
     conf = ('', port)
     server = HTTPServer(conf, RequestHandler)
@@ -41,4 +60,5 @@ def run():
     print('Closed HTTP server at port', port)
 
 
-run()
+initialize_servos()
+start_server()
